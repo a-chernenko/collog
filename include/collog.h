262ebb78dc1a1fddb8e869e6f41db7a8885894bf
Alexander Chernenko <a-chernenko@users.noreply.github.com>
2020-03-01 22:49:01 +0300

    Initial Commit of CoLor LoGger Simple Static Library

---
+++ include/collog.h
@@ -0,0 +1,151 @@
+/*
+
+MIT License
+
+Copyright (c) 2020 Alexander Chernenko (achernenko@mail.ru)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#pragma once
+
+#if __cplusplus < 201103L
+#error \
+    "This file requires compiler and library support for the ISO C++ 2011 standard or higher."
+#endif
+
+#include <iostream>
+#include <string>
+#ifndef CONCOL_NO_STRING_VIEW
+#include <string_view>
+#endif
+#include "concol.h"
+
+namespace collog {
+
+namespace detail {
+
+class logstream {
+#if __cplusplus < 201703L
+ static bool _muted;
+#else
+ inline static bool _muted{};
+#endif
+  bool _enabled{};
+
+ protected:
+  std::ostream _log;
+
+ public:
+  logstream(bool log_enable)
+      : logstream{(log_enable) ? std::cout.rdbuf() : nullptr} {}
+  logstream(std::streambuf *buf_ptr = {}) : _log{buf_ptr} {}
+  ~logstream() noexcept = default;
+  static void mute(bool muted) noexcept { _muted = muted; }
+  static bool is_mute() noexcept { return _muted; }
+  template <typename Type>
+  logstream &operator<<(Type &&rhs) {
+    if (!_muted && _enabled) {
+      _log << std::forward<Type>(rhs);
+    }
+    return *this;
+  }
+  logstream &operator<<(std::string &&rhs) {
+    if (!_muted && _enabled) {
+      concol::color::printf(rhs);
+    }
+    return *this;
+  }
+#ifndef CONCOL_NO_STRING_VIEW
+
+  logstream &operator<<(const std::string_view &rhs) {
+    if (!_muted && _enabled) {
+      concol::color::printf(rhs);
+    }
+    return *this;
+  }
+#endif
+  logstream &operator<<(const char *rhs) {
+    if (!_muted && _enabled) {
+      concol::color::printf(rhs);
+    }
+    return *this;
+  }
+  void set_enabled(bool enabled) noexcept { _enabled = enabled; }
+  bool is_enabled() const noexcept { return _enabled; }
+};
+
+}  // namespace detail
+
+class color_log : public detail::logstream {
+#ifndef CONCOL_NO_STRING_VIEW
+  const std::string_view _str;
+#else
+  const std::string _str;
+#endif
+
+  void print_header() {
+    _log << concol::color_type::black_bright << "[" << _str << "] "
+         << concol::color_ctrl::reset;
+  }
+
+ public:
+#ifndef CONCOL_NO_STRING_VIEW
+  color_log(const std::string_view &str, bool enable = true)
+      : logstream((enable) ? std::cout.rdbuf() : nullptr), _str{str} {}
+#else
+  color_log(const std::string str, bool enable = true)
+      : logstream((enable) ? std::cout.rdbuf() : nullptr), _str{str} {}
+#endif
+  ~color_log() {}
+  logstream &stream{*this};
+  template <typename Type>
+  logstream &operator<<(Type &&rhs) {
+    if (!logstream::is_mute() && logstream::is_enabled()) {
+      print_header();
+      _log << std::forward<Type>(rhs);
+    }
+    return *this;
+  }
+  logstream &operator<<(std::string &&rhs) {
+    if (!logstream::is_mute() && logstream::is_enabled()) {
+      print_header();
+      concol::color::printf(rhs.c_str());
+    }
+    return *this;
+  }
+#ifndef CONCOL_NO_STRING_VIEW
+  logstream &operator<<(std::string_view &&rhs) {
+    if (!logstream::is_mute() && logstream::is_enabled()) {
+      print_header();
+      concol::color::printf(rhs.data());
+    }
+    return *this;
+  }
+#endif
+  logstream &operator<<(const char *rhs) {
+    if (!logstream::is_mute() && logstream::is_enabled()) {
+      print_header();
+      concol::color::printf(rhs);
+    }
+    return *this;
+  }
+};  // namespace InSys
+
+}  // namespace collog
---
+++ test/src/test_collog.cpp
@@ -0,0 +1,86 @@
+/*
+
+MIT License
+
+Copyright (c) 2020 Alexander Chernenko (achernenko@mail.ru)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#include "collog.h"
+
+#if __cplusplus < 201402L
+#define COLLOG_NO_STRING_LITERALS
+#endif
+
+using namespace collog;
+using namespace concol;
+using namespace concol_literals;
+#ifndef COLLOG_NO_STRING_LITERALS
+using namespace std::string_literals;
+#endif
+#ifndef CONCOL_NO_STRING_VIEW
+using namespace std::string_view_literals;
+#endif
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) try {
+  color_log log("LOG NAME", true);
+  color::set_enabled(true);
+  log.set_enabled(true);
+  log << "test: "
+      << "OK\n"_green_bright;
+  log << "test: "
+      << "ERROR\n"_red_bright;
+
+  log << color::to_string("test: {+green}OK{}\n");
+  log << color::to_string("test: {+red}ERROR{}\n");
+
+#ifndef CONCOL_NO_STRING_VIEW
+  log << "test: " << color_type::green_bright << "OK\n"sv;
+  log << "test: " << color_type::red_bright << "ERROR\n"sv;
+#endif
+  log << color::to_string("{+white}value{}: {+yellow}%d{}\n", 128);
+  log << "{+white}value{}: " << color_type::yellow_bright << 128
+      << color_ctrl::reset << '\n';
+
+  log << color::to_string("{+white}value{}: {+yellow}%.2f{}\n", 3.14);
+  log << "{+white}value{}: " << color_type::yellow_bright << 3.14
+      << color_ctrl::reset << '\n';
+
+  color::set_enabled(false);
+  log << "noncolor\n"_red_bright;
+
+  log.set_enabled(false);
+  log << "log is disable\n";
+
+  log.set_enabled(true);
+  log << "log is enable\n";
+
+  collog::color_log::mute(true);
+  log << "log is muted\n";
+  collog::color_log::mute(false);
+
+  color::set_enabled(true);
+  log << "{+white}message {+cyan}with header ";
+  log.stream << "{+white}and message {+yellow}without header{}\n";
+  return 0;
+} catch (...) {
+  std::cerr << "\nunexpected exception\n";
+  return 0;
+}
